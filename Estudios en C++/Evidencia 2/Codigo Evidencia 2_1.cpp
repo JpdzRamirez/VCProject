939be10e987d07e570adde68d13db23e0d65b265
ramires32 <jeremyivanpedraza@gmail.com>
2021-03-23 14:06:56 -0500

    Actualiza de .cpp

--- Estudios en C++/Evidencia 2/Codigo Evidencia 2_1.cpp
+++ Estudios en C++/Evidencia 2/Codigo Evidencia 2_1.cpp
@@ -2,27 +2,28 @@
 /*
 este es otro ejemplo de comentario
 */
-
-#include <iostream>
-#include <vector>
+// El tipo de comentario se opera con doble diagonal o diagonal puntero, esto permite realizar comentarios de linea 
+//o comentarios con salto de linea contenidos entre puntero diagonal.
+#include <iostream> // Se importan las librerias las cuales continene la intelligencia , variables declaradas y funciones para agilizar 
+#include <vector> //   el desarrollo del algoritmo.
 #include <string>
 
-using namespace std;
+using namespace std;   // Se define std para abreviar la de claración de las funcioens "cin y cout".
 
 int main()
 {
-int a, b;
- cout << "Ingresa el primer numero"<<endl;
- cin >> a;
+int a, b;  // Se decaran las variables enteras para ingresar los numeros utilizados para los calculos en el algoritmo.
+ cout << "Ingresa el primer numero"<<endl;// Se utiliza cout para imprimir el mensaje en la pantalla de la terminal, se hace un salto de linea
+ cin >> a; // Se utiliza el cin para solicitar entrada de un numero entero, si se ingresa una letra esta se traduce a factor numerico segun el ASCII
  cout << "Ingresa el segundo numero"<<endl;
  cin >> b;
- cout <<"La suma de los numeros es: "<< a+b <<endl;
+ cout <<"La suma de los numeros es: "<< a+b <<endl; //Se imprimen los mensajes en la pantalla y se realizan operaciones con los numeros
  cout <<"La resta de los numeros es: "<< a-b <<endl;
  cout <<"La multiplicacion de los numeros es: "<< a*b <<endl;
  cout <<"La division de los numeros es: "<< a/b <<endl;
  cout <<"El residuo es: "<< a%b <<endl;
 
- system("pause");
+ system("pause"); // Se genera un apausa para evitar que finalice el programa hasta generar la orden
  return EXIT_SUCCESS;
 
 
