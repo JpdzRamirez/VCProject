aed9b2afb27612ae1f6c9400147a7281d055bf2c
ramires32 <jeremyivanpedraza@gmail.com>
2021-03-22 23:15:52 -0500

    llenado de hellow

---
+++ Videos/helloworld.cpp
@@ -0,0 +1,29 @@
+//este es un ejemplo de comentario
+/*
+este es otro ejemplo de comentario
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+int a, b;
+ cout << "Ingresa el primer numero"<<endl;
+ cin >> a;
+ cout << "Ingresa el segundo numero"<<endl;
+ cin >> b;
+ cout <<"La suma de los numeros es: "<< a+b <<endl;
+ cout <<"La resta de los numeros es: "<< a-b <<endl;
+ cout <<"La multiplicacion de los numeros es: "<< a*b <<endl;
+ cout <<"La division de los numeros es: "<< a/b <<endl;
+ cout <<"El residuo es: "<< a%b <<endl;
+
+ system("pause");
+ return EXIT_SUCCESS;
+
+
+}
