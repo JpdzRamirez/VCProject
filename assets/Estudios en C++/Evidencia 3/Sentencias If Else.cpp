f364910fb78bc824a4abfec459f49880e798fdda
ramires32 <jeremyivanpedraza@gmail.com>
2021-03-28 10:13:53 -0500

    Update Ev

--- assets/Estudios en C++/Evidencia 3/Sentencias If Else.cpp
+++ assets/Estudios en C++/Evidencia 3/Sentencias If Else.cpp
@@ -6,20 +6,19 @@ using namespace std;
 int main()
 {
     int edad;
-    cout<<"Ingrese la edad de su hijo ";
+    cout<<"Ingrese la edad de su hijo "<<endl;
     cin>> edad;
     if (edad>=0 && edad<6)
     {
-        cout<<"el niño pertenece al grupo de la primera infancia";
+        cout<<"el niño pertenece al grupo de la primera infancia"<<endl;
     }
         else if (edad>=6 && edad<12)
         {
-        cout<<" el niño pertenece al grupo de la segunda infancia";
+        cout<<" el niño pertenece al grupo de la segunda infancia"<<endl;
         }
             else
                 {
-                    cout<<"el niño pertenece al grupo de los adolescentes";
+                    cout<<"el niño pertenece al grupo de los adolescentes"<<endl;
                 }
-    
 return EXIT_SUCCESS;
 }
